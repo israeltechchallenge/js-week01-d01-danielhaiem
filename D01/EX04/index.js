1f110cffa83ec68fc40b4b939684e3e291365b6a
Daniel Haiem <dnhaiem@gmail.com>
2022-06-19 17:21:33 +0300

    First commit

--- D01/EX04/index.js
+++ D01/EX04/index.js
@@ -1,5 +1,16 @@
-function modulo() {
+function modulo(num1, num2) {
   //your code
+  let result = num1 % num2;
+  if (num1 > num2 && result != 0) {
+    console.log(`The modulo of ${num1} % ${num2} is ${result}`);
+    console.log(`The number ${num1} is bigger than ${num2}`);
+  } else if (num1 > num2 && result === 0) {
+    console.log(`The number ${num1} is bigger than ${num2}`);
+  } else if (num1 === num2) {
+    console.log(`The number ${num1} is equal to ${num2}`);
+  } else {
+    console.log(`The number ${num1} is less than ${num2}`);
+  }
 }
 
 // Do not remove or change this line, or the tests won't work
--- D01/EX06/index.js
+++ D01/EX06/index.js
@@ -1,3 +1,19 @@
+const outputEl = document.getElementById("output");
+
 function moduloEx6(number, comparer) {
   //your code
+  let result = number % comparer;
+  let answer = "";
+  if (number > comparer && result != 0) {
+    answer = `The number ${number} is bigger than ${comparer}. But the modulo of ${number} % ${comparer} is ${result}`;
+  } else if (number > comparer && result === 0) {
+    answer = `The number ${number} is bigger than ${comparer}`;
+  } else if (number === comparer) {
+    answer = `The number ${number} is equal to ${comparer}`;
+  } else {
+    answer = `The number ${number} is less than ${comparer}`;
+  }
+  outputEl.innerText = answer;
 }
+
+moduloEx6(8, 3);
